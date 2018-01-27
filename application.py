f142322bccb1316d17042d2e201f8186fc17dc0d
annie-qiu26 <30778029+annie-qiu26@users.noreply.github.com>
2018-01-27 16:44:39 -0600

    Fixed the database for login

--- application.py
+++ application.py
@@ -2,6 +2,7 @@ from flask import Flask, flash, redirect, render_template, request, session, abo
 from forms import RegisterForm, LoginForm
 from init import app
 from models import db, User
+from flask_login import LoginManager, login_user, login_required, logout_user
 
 
 def init_db():
@@ -9,11 +10,22 @@ def init_db():
     db.app = app
     db.create_all()
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(username):
+    return User.query.filter_by(username = username).first()
+
 
 @app.route("/")
 def main():
     return render_template('index.html')
 
+@app.route('/protected')
+@login_required
+def protected():
+    return "Home Page for Users"
 
 @app.route("/createProject")
 def testing():
@@ -34,7 +46,17 @@ def login():
     if login.validate_on_submit():
         name = login.name.data
         password = login.password.data
-    return render_template('login.html', form=login, name=name, password=password)
+        user = User.query.filter_by(username=LoginForm.name.data).first()
+        if user:
+            if user.password == LoginForm.password.data:
+                login_user(user)
+                return "User logged in"
+            else:
+                return "Wrong password"
+        else:
+            return "User does not exist"
+    else:
+        return render_template('login.html', form=login, name=name, password=password)
 
 
 @app.route("/register", methods=['GET', 'POST'])
@@ -50,16 +72,24 @@ def register():
         if User.query.filter_by(email=RegisterForm.email.data).first():
             return render_template('register.html', form=register)
         else:
-            newuser = User(RegisterForm.email.data, RegisterForm.password.data)
+            newuser = User(RegisterForm.email.data, RegisterForm.username.data, RegisterForm.password.data)
             db.session.add(newuser)
             db.session.commit()
             username = register.username.data
             password = register.password.data
-            return render_template('register.html', form=register, email=email, firstname=firstname, lastname=lastname,
+            email = register.email.data
+            login_user(newuser)
+            return render_template('createProject.html', form=register, email=email, firstname=firstname, lastname=lastname,
                                    username=username, password=password)
     else:
         return render_template('register.html', form=register)
 
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return "Logged out"
 
 if __name__ == "__main__":
     init_db()
