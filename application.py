1e409412614e7a9038b06f6771a9511dad9555e7
Athena Braun <braun01@college.harvard.edu>
2018-01-26 23:21:24 -0500

    testing routes

--- application.py
+++ application.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 def main():
     return "Welcome"
 
+
+@app.route("/testing")
+def testing():
+    return render_template("template.html")
+
 if __name__ == "__main__":
     app.run()
 
-    
